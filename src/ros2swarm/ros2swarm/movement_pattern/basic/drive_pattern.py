c961b31dff5dde272da23cc7fe83755eb1cb839b
charlod <seray.arslan@uni-konstanz.de>
2023-06-12 15:30:00 +0200

    add ad-hoc network position msg

--- src/ros2swarm/ros2swarm/movement_pattern/basic/drive_pattern.py
+++ src/ros2swarm/ros2swarm/movement_pattern/basic/drive_pattern.py
@@ -1,26 +1,15 @@
-#    Copyright 2020 Marian Begemann
-#
-#    Licensed under the Apache License, Version 2.0 (the "License");
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
-#
-#        http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Point
 from ros2swarm.movement_pattern.movement_pattern import MovementPattern
 from ros2swarm.utils import setup_node
+from gazebo_msgs.msg import ModelStates
+from scipy.spatial import distance
 
 
 class DrivePattern(MovementPattern):
     """
     A simple pattern for driving a constant direction vector.
 
-    Which is configured in with the parameters of this pattern.
+    Which is configured with the parameters of this pattern.
     How often the direction is published is configured in the timer period parameter.
     """
 
@@ -44,24 +33,54 @@ class DrivePattern(MovementPattern):
         self.param_z = float(
             self.get_parameter("drive_angular").get_parameter_value().double_value)
 
-        self.get_logger().warn('Logger is: ' + self.get_logger().get_effective_level().name)
-        self.get_logger().info('Logger is: info ')
-        self.get_logger().debug('Logger is: debug')
+        self.robot_positions = {}  # Store the position of each robot
+
+        self.model_states_subscription = self.create_subscription(
+            ModelStates,
+            '/gazebo/model_states',
+            self.model_states_callback,
+            10
+        )
+
+    def model_states_callback(self, msg):
+        for i, name in enumerate(msg.name):
+            if name.startswith('robot_name'):  # Check if the model name is a robot
+                self.robot_positions[name] = [msg.pose[i].position.x, msg.pose[i].position.y, msg.pose[i].position.z]
+
+    def find_closest_neighbors(self, robot_name, n=None, r=None):
+        """Find the n closest neighbors to the given robot within radius r."""
+        if robot_name not in self.robot_positions:
+            return []
+
+        robot_position = self.robot_positions[robot_name]
+        distances = []
+
+        for name, position in self.robot_positions.items():
+            if name != robot_name:
+                dist = distance.euclidean(robot_position, position)
+                if r is None or dist <= r:
+                    distances.append((name, dist))
+
+        distances.sort(key=lambda x: x[1])  # Sort by distance
+
+        if n is not None:
+            return distances[:n]  # Return the n closest neighbors
+        else:
+            return distances
 
     def timer_callback(self):
         """Publish the configured twist message when called."""
-
         msg = Twist()
-        # command to publish the message in the terminal by hand
-        # ros2 topic pub --once /cmd_vel geometry_msgs/msg/Twist "{
-        # linear: {x: 0.26, y: 0.0, z: 0.0},
-        # angular: {x: 0.0, y: 0.0, z: 0.0}
-        # }"
         msg.linear.x = self.param_x
         msg.angular.z = self.param_z
         self.command_publisher.publish(msg)
         self.get_logger().debug('Publishing {}:"{}"'.format(self.i, msg))
+
         self.i += 1
+
+        for name in self.robot_positions.keys():
+            neighbors = self.find_closest_neighbors(name, n=3)  # Find the 3 closest neighbors
+            self.get_logger().info('Closest neighbors to {}: {}'.format(name, neighbors))
 
 
 def main(args=None):
